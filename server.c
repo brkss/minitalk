26e99e7624528b2f709d0431124554228580ce8b
brkss <berkassebrahim@gmail.com>
2021-12-09 21:59:14 +0100

    use struct instead of two global variables !

--- server.c
+++ server.c
@@ -6,68 +6,67 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 01:53:23 by bberkass          #+#    #+#             */
-/*   Updated: 2021/12/09 21:10:21 by bberkass         ###   ########.fr       */
+/*   Updated: 2021/12/09 21:58:32 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/minitalk.h"
-/*
+
 typedef struct ch_s {
 	int c;
 	int shft;
 } ch_t;
 
 ch_t *gv;
-*/
 
-int c = 0b00000000;
-int shft = 0;
+
+//int c = 0b00000000;
+//int shft = 0;
 
 int reverse(int b) {
-   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+   b = (b & 0b11110000) >> 4 | (b & 0b00001111) << 4;
+   b = (b & 0b11001100) >> 2 | (b & 0b00110011) << 2;
+   b = (b & 0b10101010) >> 1 | (b & 0b01010101) << 1;
    return b;
 }
 
 void	handl(int s)
 {
+	char	t;
+
 	if(s == SIGUSR1)
 	{
 		// 0
-		c = c | 0;
+		gv->c = gv->c | 0;
 	}
 	else if(s == SIGUSR2)
 	{
 		// 1
-		c = c | 1;
+		gv->c = gv->c | 1;
 	}
 	else {
 		//printf("unknown signal ! \n");
 	}
-	if(shft < 7)
-		c = c << 1;
-	shft++;
-	if(shft == 8)
+	if(gv->shft < 7)
+		gv->c = gv->c << 1;
+	gv->shft++;
+	if(gv->shft == 8)
 	{
-		c = reverse(c);
-		char t = (char) c;
+		gv->c = reverse(gv->c);
+		t = (char)(gv->c);
 		write(1, &t, 1);
-		//printf("%c", (char)(c));
-		c = 0b00000000;
-		shft = 0;
+		gv->c = 0b00000000;
+		gv->shft = 0;
 	}
-	//gv->shft = gv->shft + 1 - 1;
-	//printf("%d \n", shft);
 }
 
 int main()
 {
-/*	
+	
 	gv = (ch_t *)malloc(sizeof(ch_t));
 	gv->c = 0b00000000;
 	gv->shft = 0;
-*/
+
 	pid_t pid;
 	signal(SIGUSR1, handl);
 	signal(SIGUSR2, handl);
