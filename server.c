50d614ac684c72a9b5bc20324f5640c29a192d76
brkss <berkassebrahim@gmail.com>
2021-12-12 19:51:25 +0100

    📙 dd useless functions !

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 01:53:23 by bberkass          #+#    #+#             */
-/*   Updated: 2021/12/12 19:41:24 by bberkass         ###   ########.fr       */
+/*   Updated: 2021/12/12 19:50:55 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,26 @@
 
 ch_t *gv;
 
-int reverse(int b) {
-   b = (b & 0b11110000) >> 4 | (b & 0b00001111) << 4;
-   b = (b & 0b11001100) >> 2 | (b & 0b00110011) << 2;
-   b = (b & 0b10101010) >> 1 | (b & 0b01010101) << 1;
-   return b;
-}
-
 void	handl(int s)
 {
 	char	t;
 
 	if (s == SIGUSR1)
 	{
-		// 0
 		gv->c = gv->c | 0;
 	}
 	else if (s == SIGUSR2)
 	{
-		// 1
 		gv->c = gv->c | 1;
 	}
 	else {
-		//printf("unknown signal ! \n");
+		write(1, "unkown signal ! \n", 18)
 	}
 	if (gv->shft < 7)
 		gv->c = gv->c << 1;
 	gv->shft++;
 	if (gv->shft == 8)
 	{
-		//gv->c = reverse(gv->c);
 		t = (char)(gv->c);
 		write(1, &t, 1);
 		gv->c = 0b00000000;
@@ -63,7 +53,7 @@ int main()
 	signal(SIGUSR2, handl);
 	pid = getpid();
 	printf("PID => %d \n", pid);
-	while(1)
+	while (1)
 	{
 		sleep(1);
 	}
